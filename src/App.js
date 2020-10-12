9799535ad6826d942b706c3e4cdd5bc8f837dd97
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-10-12 17:22:05 +0530

    Added preview page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
-
-const HatsPage =() => (
-  <div>
-    <h1>Hats Page Bitch!</h1>
-  </div>
-);
+import ShopPage from './page/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/shop/hats' component={HatsPage}></Route>
+      <Route exact path='/shop' component={ShopPage}></Route>
     </div>
   );
 }
