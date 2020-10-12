3108346f1eba1a640f2fd3eb81234aea93cadec1
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-10-12 14:53:56 +0530

    Router added with hats page!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
+
+const HatsPage =() => (
+  <div>
+    <h1>Hats Page Bitch!</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route exact path='/shop/hats' component={HatsPage}></Route>
     </div>
   );
 }
